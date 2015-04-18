53f225e57f08b01ea4355c18ca1f7c7ba355c25a
celsoabreu <celso.abreu@gmail.com>
2015-04-17 21:56:49 -0300

    Update views.py

--- views.py
+++ views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
+from django.http import Http404
 from django.shortcuts import render_to_response, render, get_object_or_404
 from django.template import RequestContext
-from django.http import Http404
+
 
 
 from models import Itemagenda
@@ -31,15 +32,17 @@ def transacao(request):
 	)
 
 
-
 def	listacli(request):
 	lista_clientes = Cliente.objects.all()
 	return render_to_response("lista.html", {'lista_clientes': lista_clientes },
 		context_instance=RequestContext(request))
-	lista_itens = Itemagenda.objects.all()
-	return render_to_response("lista.html", {'lista_itens': lista_itens },
-		context_instance=RequestContext(request))
 
+	try:
+		lista_item = Itemagenda.objects.all()
+	except Itemagenda.DoesNotExist:
+		raise Http404(u"Tabela sem dados !")
+	return render_to_response("lista.html", {'lista_item': lista_item },
+		context_instance=RequestContext(request))
 
 def	adccliente(request):
 	if request.method == "POST":
@@ -55,7 +58,7 @@ def	adccliente(request):
 			
 def Itemagenda(request):
 	if request.method == "POST":
-		form = Itemagenda(request.POST, request.FILES)
+		form = FormItemagenda(request.POST, request.FILES) 
 		if form.is_valid():
 			form.save()
 			return render_to_response("salvo.html", {},  )
@@ -70,7 +73,6 @@ def Detalhe_cli(request, nr_cli):
 		form = Cliente.objects.get(pk=nr_cli)
 	except Cliente.DoesNotExist:
 		raise Http404(u"Cliente nao existe !")
-
 	return render_to_response("detalhe_cli.html", {'form': form},
 		context_instance=RequestContext(request))
 
@@ -79,4 +81,5 @@ def Detalhe_item(request, nr_item):
 	item = get_object_or_404(Itemagenda,pk=nr_item)
 	return render_to_response("detalhe_item.html", {'item': item},
 		context_instance=RequestContext(request))
+
 
